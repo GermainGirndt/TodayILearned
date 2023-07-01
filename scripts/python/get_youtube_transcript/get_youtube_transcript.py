d83ca2f8a9a7034e597ab301381c45c26d561e9d
Germain Girndt <germaingirndt@outlook.com>
2023-07-01 21:56:17 +0200

    Added python script for getting transcript

---
+++ scripts/python/get_youtube_transcript/get_youtube_transcript.py
@@ -0,0 +1,33 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+def get_transcript(video_url):
+    # Extract the video ID from the URL
+    video_id = video_url.split("watch?v=")[1]
+
+    # Fetch the transcript
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    except Exception as e:
+        print("An error occurred while fetching the transcript:", str(e))
+        return None
+
+    # Combine all the parts of the transcript
+    full_transcript = ""
+    for part in transcript:
+        full_transcript += part['text'] + ' '
+
+    return full_transcript
+
+
+# put the actual URL here
+video_url = "https://www.youtube.com/watch?v=fJ9rUzIMcZQ"
+transcript = get_transcript(video_url)
+
+if transcript is not None:
+    # Write the transcript to a text file
+    with open("transcript.txt", "w") as f:
+        f.write(transcript)
+    print("Success! Transcript saved to transcript.txt")
+else:
+    print("Error: No transcript found")
