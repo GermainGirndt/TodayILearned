915b8a47fcb5192fedbca8c289b9d4a310fcc2c8
Germain Girndt <germaingirndt@outlook.com>
2023-07-25 08:12:35 +0200

    Separated input and output folders

--- scripts/python/convert_all_heic_images_to_jpg/convert_all_heic_to_jpg.py
+++ scripts/python/convert_all_heic_images_to_jpg/convert_all_heic_to_jpg.py
@@ -1,25 +1,28 @@
 # install HEIF library
 # brew install libheif
+# PIP INSTALL PILLOW
 
 import os
 from PIL import Image
-import pyheif_pillow_opener
 import subprocess
 
 
-def convert_heic_to_jpg(folder_path):
-    for filename in os.listdir(folder_path):
+def convert_heic_to_jpg(input_folder_path, output_folder_path):
+    for filename in os.listdir(input_folder_path):
         if filename.endswith(".HEIC") or filename.endswith(".heic"):
-            heic_path = os.path.join(folder_path, filename)
-            jpg_path = os.path.splitext(heic_path)[0] + ".jpg"
+            heic_path = os.path.join(input_folder_path, filename)
+            jpg_path = os.path.join(output_folder_path, filename)
 
             try:
-                subprocess.run(["sips", "-s", "format", "jpeg", heic_path, "--out", jpg_path])
+                subprocess.run(["sips", "-s", "format", "jpeg",
+                               heic_path, "--out", jpg_path])
                 print(f"Converted {heic_path} to {jpg_path}")
             except Exception as e:
                 print(f"Error converting {heic_path}: {str(e)}")
 
 
 # Example usage
-folder_path = '/Users/germaingirndt/Downloads/Fotos Modem'  # Replace with the path to your folder containing .HEIC images
-convert_heic_to_jpg(folder_path)
+# Replace with the path to your folder containing .HEIC images
+input_folder_path = './input'
+output_folder_path = './output'
+convert_heic_to_jpg(input_folder_path, output_folder_path)
