f24221115a49fa7864b43e562938afed364132b5
Germain Girndt <germaingirndt@outlook.com>
2023-09-21 14:02:18 +0200

    Improved script

--- scripts/python/show_finder/main.py
+++ scripts/python/show_finder/main.py
@@ -4,27 +4,31 @@ import requests
 
 
 locales = ["en_US",
-"en_GB",
-"en_CA",
-"en_AU",
-"fr_FR",
-"fr_CA",
-"de_DE",
-"es_ES",
-"es_MX",
-"pt_BR",
-"pt_PT",
-"zh_CN",
-"zh_TW",
-"ja_JP",
-"ko_KR",
-"ru_RU"]
+           "en_GB",
+           "en_CA",
+           "en_AU",
+           "de_DE",
+           "pt_BR",
+           "pt_PT",
+           "fr_FR",
+           "fr_CA"]
+
+locales_extended = ["es_ES",
+                    "es_MX",
+                    "zh_CN",
+                    "zh_TW",
+                    "ja_JP",
+                    "ko_KR",
+                    "ru_RU"]
+# uncomment if extend
+# locales = locales + locales_extended
+
 
 def get_movie_details(title, locale):
     base_url = "https://apis.justwatch.com/content/titles/"
 
     search_url = f"{base_url}{locale}/popular?body={{\"query\":\"{title}\",\"content_types\":[\"movie\"]}}"
-    
+
     # Search for the movie
     search_response = requests.get(search_url)
     search_data = search_response.json()
@@ -32,7 +36,7 @@ def get_movie_details(title, locale):
     # Check if there are results
     if not search_data['items']:
         return f"No results found for '{title}'."
-    
+
     # Get the first matching movie's details
     movie_id = search_data['items'][0]['id']
     details_url = f"{base_url}movie/{movie_id}/locale/{locale}"
@@ -42,7 +46,7 @@ def get_movie_details(title, locale):
     # Retrieve the offering details
     offers = details_data.get('offers')
 
-    #print(details_data)
+    # print(details_data)
 
     # Check if there are results
     if not offers:
@@ -50,18 +54,18 @@ def get_movie_details(title, locale):
 
     result = []
     for offer in offers:
-        #print(offer)
-        platform = offer['monetization_type']
-        url_pieces = offer['urls'].get('standard_web').split('/')[1:3]
-        url = "".join(url_pieces)
+        # print(offer)
+        monetization_type = offer['monetization_type']
+        url_pieces = offer['urls'].get('standard_web').split('/')[1:4]
+        url = "/".join(url_pieces)
 
+        result.append({"monetization_type": monetization_type, "url": url})
 
-        result.append({ "platform" : platform, "url": url})
-    
     return result
 
+
 # Usage
-title = "Baki"
+title = input("Insert the title: ")
 
 for locale in locales:
     print(f"\n\n{locale}:")
