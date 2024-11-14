7720c5c871a5bef0f06466fbea205f440187e52b
Germain Girndt <germaingirndt@outlook.com>
2024-11-14 17:40:41 +0100

    Extended functionality

--- scripts/python/pdf_extract_pages/main.py
+++ scripts/python/pdf_extract_pages/main.py
@@ -1,16 +1,23 @@
 import PyPDF2
+from PyPDF2 import PdfMerger
 import uuid
 
 
-def extract_pdf_pages(pdf_filename, ranges):
+def extract_pdf_pages(pdf_filename, ranges, merge: bool = False):
+
+    filenames = []
+
     with open(pdf_filename, 'rb') as pdf_file:
         reader = PyPDF2.PdfReader(pdf_file)
 
         for range_ in ranges:
             start_page, end_page = range_
-            if start_page < 1 or end_page > len(reader.pages):
+            if start_page < 1 or end_page > len(reader.pages) or end_page < -1:
                 print(f"Invalid range {start_page}-{end_page}. Skipping.")
                 continue
+
+            if end_page == -1:
+                end_page = len(reader.pages)
 
             writer = PyPDF2.PdfWriter()
             for page_num in range(start_page - 1, end_page):
@@ -23,15 +30,30 @@ def extract_pdf_pages(pdf_filename, ranges):
 
             print(
                 f"Extracted pages {start_page}-{end_page} to {unique_filename}")
+            filenames.append(unique_filename)
+
+    if merge:
+        merger = PdfMerger()
+        for filename in filenames:
+            merger.append(filename)
+
+        merged_filename = f"output/merged_extracted_pdf_{uuid.uuid4().hex}.pdf"
+        merger.write(merged_filename)
+        merger.close()
+
+        print(f"Merged all extracted PDFs into {merged_filename}")
 
 
 if __name__ == "__main__":
     # Replace this with your PDF file name
-    pdf_filename = "input/projektarbeit.pdf"
+    pdf_filename = "input/2023- Recibo e Declaracao Imposto de Renda - Rodrigo - Exercicio 2023 - Calendario 2022.pdf"
     ranges = [
-        [28, 42],  # This will extract pages 1 to 3
-        [43, 49],  # This will extract pages 5 to 7
-        # Add more ranges as needed
+        [1, 1],  # This will extract pages 1 to 10
+        [3, -1]  # Add more ranges as needed
     ]
 
-    extract_pdf_pages(pdf_filename, ranges)
+    extract_pdf_pages(
+        pdf_filename=pdf_filename,
+        ranges=ranges,
+        merge=True
+    )
